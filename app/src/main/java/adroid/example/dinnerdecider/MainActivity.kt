dcf9ae875d0c328f0f3b81849f85d4c8cd1007b5
Page V Petty <pagevpetty@live.com>
2019-03-25 23:32:15 -0600

    add a bunch to layout

--- app/src/main/java/adroid/example/dinnerdecider/MainActivity.kt
+++ app/src/main/java/adroid/example/dinnerdecider/MainActivity.kt
@@ -13,7 +13,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        selectedFoodTxt.text = "Costa Vida"
     }
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
